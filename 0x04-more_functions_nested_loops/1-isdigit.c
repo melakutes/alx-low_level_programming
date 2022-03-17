16c930c8b1a12e4958ab616aa003fedf0ffe4e16
melakutes <melakutessema04@gmail.com>
2022-03-17 11:58:26 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code for ALX students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+return (c >= 48 && c <= 57);
+}
