9ba3cf6f4b8cbe895d0cb41309bc7af1f3e257ad
melakutes <melakutessema04@gmail.com>
2022-03-17 12:18:55 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - check the code for ALX students.
+ *
+ * @a: is an integer param
+ *
+ * @b: is an integer param
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
