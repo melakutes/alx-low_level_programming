1e61c8babf83a5fe32271d60e66bc270a09e437e
melakutes <melakutessema04@gmail.com>
2022-03-17 13:33:21 -0700

    more number 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,45 +1,28 @@
 #include "main.h"
 
 /**
- * print_line - prints n
- * @n: size
- * Return: none.
- */
-
-void print_line(int n)
-{
-while (n > 0)
-{
-_putchar('_');
-n--;
-}
-_putchar('\n');
-}
-#include "main.h"
-
-/**
  * more_numbers - more ns..
  * Return: 0
  */
 
 void more_numbers(void)
 {
-char i, j, k;
-int l;
+	char i, j, k;
+	int l;
 
-for (l = 0; l <= 9; l++)
-{
-k = '9';
-for (i = '0'; i <= '1'; i++)
-{
-for (j = '0'; j <= k; j++)
-{
-if (i > '0')
-_putchar(i);
-_putchar(j);
-}
-k = '4';
-}
-_putchar('\n');
-}
+	for (l = 0; l <= 9; l++)
+	{
+		k = '9';
+		for (i = '0'; i <= '1'; i++)
+		{
+			for (j = '0'; j <= k; j++)
+			{
+				if (i > '0')
+					_putchar(i);
+				_putchar(j);
+			}
+			k = '4';
+		}
+		_putchar('\n');
+	}
 }
