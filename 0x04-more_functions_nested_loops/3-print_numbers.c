b4f069337f2c948d02f08319805ad1851107e073
melakutes <melakutessema04@gmail.com>
2022-03-17 13:00:12 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code for ALX students.
+ *
+ *
+ * Return: No.
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
