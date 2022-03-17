ec892267d9dc72b5816125e6712678d775cba460
melakutes <melakutessema04@gmail.com>
2022-03-17 13:05:56 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code for ALX students.
+ *
+ *
+ * Return: No.
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if (i != 50 && i != 52)
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
