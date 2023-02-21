e5c844902e2f271c0f1aacd10871cfbfeccbcef5
melakutes <melakutessema04@gmail.com>
2023-02-21 04:36:44 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+int _putchar(char c);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int i;
+char mt[9] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar (mt[i]);
+	}
+_putchar ('\n');
+return (0);
+}
