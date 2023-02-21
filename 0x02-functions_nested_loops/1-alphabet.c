5026dc19869377498066230e7a48e8a97f97c62d
melakutes <melakutessema04@gmail.com>
2023-02-21 04:50:34 -0800

    print alphabet a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet -code for ALX students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+
+}
