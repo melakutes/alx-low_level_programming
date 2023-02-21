36f2a89fa248f051488da6ce31321dfe885977c3
melakutes <melakutessema04@gmail.com>
2023-02-21 05:54:37 -0800

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int _putchar(char c);
+
+/**
+ * times_table - print 9 table
+ *
+ * Return: No return
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int n = 9;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if ((i * j) < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((j * i) + '0');
+			}
+			else if ((i * j) < 100)
+			{
+				_putchar(' ');
+				_putchar((((j * i) / 10) % 10) + '0');
+				_putchar(((j * i) % 10) + '0');
+			}
+			if (j != (n))
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
