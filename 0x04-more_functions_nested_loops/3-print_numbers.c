dec3dd714634b8a667df82e53ca5b7dd43b64a90
melakutes <melakutessema04@gmail.com>
2023-02-23 13:37:09 -0800

    3-print_numbers DONE _putchar.c added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code for ALX School students.
+ *
+ *
+ * Return: No.
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
