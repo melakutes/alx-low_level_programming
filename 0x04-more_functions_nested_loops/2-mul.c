ffb366105f57434550cd069209e4f82058fde491
melakutes <melakutessema04@gmail.com>
2023-02-23 13:31:05 -0800

    2-mul done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - check the code for ALX School students.
+ *
+ * @a: is an integer param
+ *
+ * @b: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
