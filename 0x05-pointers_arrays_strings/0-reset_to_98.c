3a6450916d1f331518d9e6071f264d4ac2bc42d2
melakutes <melakutessema04@gmail.com>
2023-02-27 18:22:17 -0800

    change the pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: int whose value is to be replaced by 98
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+extern void reset_to_98(int *n);
+extern void swap_int(int *a, int *b);
+extern int _strlen(char *s);
+extern void _puts(char *str);
+extern void print_rev(char *s);
+extern void rev_string(char *s);
+extern void puts2(char *str);
+extern void puts_half(char *str);
+extern void _putchar(char c);
+extern void print_array(int *a, int n);
+extern char *_strcpy(char *dest, char *src);
+
+#endif
