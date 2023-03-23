f70924d11c7464da20353b6b6c32155cf4c65fc5
melakutes <melakutessema04@gmail.com>
2023-03-23 13:47:02 -0700

    modified to make betty style req

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,42 +9,42 @@
 
 void print_all(const char * const format, ...)
 {
-  int i = 0;
-  char *str, *sep = "";
+	int i = 0;
+	char *str, *sep = "";
 
-  va_list list;
+	va_list list;
 
-  va_start(list, format);
+	va_start(list, format);
 
-  if (format)
-    {
-      while (format[i])
+	if (format)
 	{
-	  switch (format[i])
-	    {
-	    case 'c':
-	      printf("%s%c", sep, va_arg(list, int));
-	      break;
-	    case 'i':
-	      printf("%s%d", sep, va_arg(list, int));
-	      break;
-	    case 'f':
-	      printf("%s%f", sep, va_arg(list, double));
-	      break;
-	    case 's':
-	      str = va_arg(list, char *);
-	      if (!str)
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+		printf("%s%c", sep, va_arg(list, int));
+		break;
+		case 'i':
+		printf("%s%d", sep, va_arg(list, int));
+		break;
+		case 'f':
+		printf("%s%f", sep, va_arg(list, double));
+		break;
+		case 's':
+		str = va_arg(list, char *);
+		if (!str)
 		str = "(nil)";
-	      printf("%s%s", sep, str);
-	      break;
-	    default:
-	      i++;
-	      continue;
-	    }
-	  sep = ", ";
-	  i++;
+		printf("%s%s", sep, str);
+		break;
+		default:
+		i++;
+		continue;
+		}
+		sep = ", ";
+		i++;
 	}
-    }
-  printf("\n");
-  va_end(list);
+	}
+	printf("\n");
+	va_end(list);
 }
